ca6e1d5ee5adf1614a1afb8b7751becc3c0a4a57
takeuchi-y <takeuchi.y@opst.co.jp>
2020-02-27 17:01:11 +0900

    暗号化 初期化ベクトル格納場所を追加するコマンドを追加した

---
+++ app/Console/Commands/EncryptIV.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+/**
+ * 暗号化 初期化ベクトル配置場所を勉強するためのコマンド
+ */
+class EncryptIV extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ecnrypt-iv';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'study encrypt initial vector';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $origin = "オリジナルメッセージ";
+        $this->info("origin: {$origin}");
+        
+        $encrpted = encrypt($origin);
+        $this->info("encrpted: {$encrpted}");
+        
+        $base64Decoded = base64_decode($encrpted);
+        $this->info("base64Decoded: {$base64Decoded}");
+    }
+}
